8bc26c259048e9205694318dec2d8298cf4c5049
Mohammed.Salem <Mohammed.Salem@MOHAMMEDSALEM>
2025-05-01 00:21:11 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Islam's Carpet Cleaning Service");
+            Console.WriteLine("charges : $25 per small , $35 per large | Sales Tax = 6%" );
+            Console.WriteLine("--------");
+
+            Console.WriteLine("Estimage for carpet cleaning service");
+            Console.WriteLine("Number of small carpets : ");
+            int? smallCarp  = Convert.ToInt32(Console.ReadLine()) ;
+
+            Console.WriteLine("Number of large carpets : ");
+            int? largeCarp = Convert.ToInt32(Console.ReadLine());
+
+            int totalSmall =Convert.ToInt32(smallCarp * 25) ;
+            int totalLarge = Convert.ToInt32(largeCarp * 35);
+
+            Console.WriteLine($"Cost of small carpet is {totalSmall}");
+            Console.WriteLine($"Cost of large carpet is {totalLarge}");
+
+            int costBeforeTax = totalSmall + totalLarge;
+
+            Console.WriteLine($"total cost before tax : {costBeforeTax} ");
+
+            decimal tax = costBeforeTax * 6 / 100m;
+            Console.WriteLine($"Sales tax (6%) : {tax} ");
+            Console.WriteLine($"total cost after tax : {costBeforeTax + tax } ");
+        }
+    }
+}
---
+++ firstProject/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace firstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Hello Eraasoft 517");
+            //
+            //int[] arr = [0,0,0,0,0,0,0,0,0,0];
+            //arr[0] = 100;
+            //arr[^1] = 1000;
+            //Console.WriteLine(arr[0]);
+            //Console.WriteLine(arr[^1]);
+            //Console.WriteLine(arr[3]);
+
+
+           //Console.WriteLine("Enter your favourit number between 1 and 100 :");
+           //int? fnumber = Convert.ToInt32(Console.ReadLine()) ;
+           //Console.WriteLine($"No really !! , {fnumber} is my favourite too" );
+
+            Console.WriteLine("Enter your name");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("Enter your salary");
+            int? salary = Convert.ToInt32(Console.ReadLine()) ;
+
+            salary = salary + 1000;
+
+            // use concatenation
+            Console.WriteLine("hello " + name);
+
+            //or use string interpolation
+
+            Console.WriteLine($"hello  {name} ");
+
+
+
+        }
+    }
+}
